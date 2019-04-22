b579b52e3dcd1cbc2583159785c9f99b29f3f6af
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-04-22 09:58:41 -0700

    add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import Filter from './Filter/Filter';
 import ResultsList from './ResultsList/ResultsList';
 
 class App extends Component {
+  state={
+    items:[{
+      title:'',
+      author:[],
+      price:'',
+      description:'',
+      image:'',
+      id:''
+    }]
+  }
+  // filterState=()=>{
+  // }
+  componentDidMount() {
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=';
+    const key = 'AIzaSyA3UIsSPkNxyAoEtMa2EQ_DdHwxfsFysw8';
+    const options = {
+      method: 'GET',
+    }};
   render() {
     return (
       <div className="App">
